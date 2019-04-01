19762dc76d5bc4e20d905099bbd97c85e4c97fad
Simon Wilson <simon@net5.com.au>
2019-04-01 21:55:39 +1000

    Feat: Adds framework to build with Carthage

---
+++ DirectoryWatchdog.h
@@ -0,0 +1,18 @@
+//
+//  DirectoryWatchdog.h
+//  DirectoryWatchdog
+//
+//  Created by Simon Wilson on 1/4/19.
+//  Copyright © 2019 Simon Wilson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DirectoryWatchdog.
+FOUNDATION_EXPORT double DirectoryWatchdogVersionNumber;
+
+//! Project version string for DirectoryWatchdog.
+FOUNDATION_EXPORT const unsigned char DirectoryWatchdogVersionString[];
+
+// Public headers: 
+#import <DirectoryWatchdog/SGDirWatchdog.h>
